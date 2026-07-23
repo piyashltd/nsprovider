05d75bbe60c9a0bd48e3c0071d11439a93fb63cf
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-07-23 22:10:30 +0600

    Add buildscript configuration for Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // Cloudstream Plugin Dependency
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+    }
+}
