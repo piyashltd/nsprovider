2252afe97c018562d8f7f5ddae70ea03c5839693
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-07-23 22:43:11 +0600

    Rename build.gradle to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.plugins")
+}
+
+cloudstream {
+    setRepoName("My JSON Extension")
+    setRepoDescription("Custom API based Cloudstream Extension")
+    setRepoAuthor("You")
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:-SNAPSHOT")
+}
