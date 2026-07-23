905f174af2f15c4d19bfc92373d18648a0ff1bea
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-07-23 23:25:27 +0600

    Update settings.gradle.kts for plugin management
    Added gradlePluginPortal to pluginManagement repositories and configured resolutionStrategy for specific plugins.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,20 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven("https://jitpack.io")
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.lagradost.cloudstream3.plugins")) {
+                // এখানে master-SNAPSHOT দেওয়া হয়েছে ব্রোকেন ক্যাশ বাইপাস করার জন্য
+                useModule("com.github.recloudstream:gradle:master-SNAPSHOT")
+            }
+        }
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -14,4 +23,5 @@ dependencyResolutionManagement {
         maven("https://jitpack.io")
     }
 }
+
 include(":app")
